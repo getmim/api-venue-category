46f18427e1adb62eff6154e664912f984c533ccf
Iqbal Fauzi <iqbalfawz@gmail.com>
2019-10-09 11:22:17 +0700

    Add lib pagination as required dependency

--- modules/api-venue-category/config.php
+++ modules/api-venue-category/config.php
@@ -26,6 +26,9 @@ return [
             ],
             [
                 'api' => NULL
+            ],
+            [
+                'lib-pagination' => NULL
             ]
         ],
         'optional' => []
